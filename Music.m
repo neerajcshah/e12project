1ce56f8871e588ac3b9b5529b11ae4fbf20c5347
Ryan Jobson <rjobson1@swarthmore.edu>
2017-05-15 17:00:43 -0400

    Finished song

--- Music.m
+++ Music.m
@@ -7,9 +7,12 @@ G = 8;
 A = 10; 
 B = 12; 
 C5 = 13; 
-D5 = 15
+D5 = 15;
 E5 = 17;
-song = [C5, C5, C5, C5, B, A, B, C5, D5, E5, E5, E5];
+F5 = 18;
+G5 = 20; 
+A5 = 22;
+song = [C5, C5, C5, C5, B, A, B, C5, D5, E5, E5, E5, E5, D5, C5, D5, E5, F5, G5, C5, A5, G5, F5, E5, D5, C5];
 for i = 1:length(song)
     sound(file, Fs * notes(song(i)));
     pause(1) 
